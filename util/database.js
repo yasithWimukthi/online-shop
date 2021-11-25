9dfddd68a682e9284333c984199e72a5af7fc9bb
yasith wimukthi <it19966922@my.sliit.lk>
2021-11-25 09:42:52 +0530

    add sql database

--- util/database.js
+++ util/database.js
@@ -1,10 +1,8 @@
-const mysql = require('mysql2');
+const Sequelize = require('sequelize');
 
-const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    database: 'eshop',
-    password: ''
+const sequelize = new Sequelize('database', 'user name', 'password', {
+  dialect: 'mysql',
+  host: 'localhost'
 });
 
-module.exports = pool.promise();
+module.exports = sequelize;
